e490876b4604dee7a18011413c59545804c8790a
chil_da <1724803128@qq.com>
2024-11-08 12:58:51 +0800

    修BUG

--- code/back/src/main/java/com/douyuehan/doubao/mapper/BigscreenMapper/CityAttributesMapper.java
+++ code/back/src/main/java/com/douyuehan/doubao/mapper/BigscreenMapper/CityAttributesMapper.java
@@ -11,8 +11,15 @@ import java.util.List;
 @Mapper
 public interface CityAttributesMapper extends BaseMapper<CityAttributes> {
     List<CityAttributes> getSelectCities(List<String> cities);
-
+    @Select("<script>" +
+            "SELECT * FROM cityattributes " +
+            "WHERE cityName IN " +
+            "<foreach item='cityName' collection='cityNames' open='(' separator=',' close=')'>" +
+            "#{cityName}" +
+            "</foreach>" +
+            "</script>")
     List<CityAttributes> selectCitiesByNames(@Param("cityNames") List<String> cityNames);//用于根据用户选择的城市名显示这些城市的各项指标
+    
     @Insert("INSERT INTO cityattributes (cityName, mediatr, network, searchin,social,tourism) VALUES (#{cityName}, #{mediatr}, #{network}, #{searchin},#{social},#{tourism})")
     int insertCity(@Param("searchin") float searchin, @Param("network") float network, @Param("mediatr") float mediatr, @Param("tourism") float tourism, @Param("social") float social,@Param("cityName") String cityName);
 
